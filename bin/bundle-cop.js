4671356f2055c03c7963f74ec98225543e298332
hanford <jackhanford@gmail.com>
2017-12-11 23:09:44 -0800

    remove screenshot, clean up code

--- bin/bundle-cop.js
+++ bin/bundle-cop.js
@@ -9,27 +9,24 @@ const chalk = require('chalk')
 const fs = require('mz/fs')
 const SimpleGit = require('simple-git')
 
-const screenshot = require('./screenshot')
-
 const log = console.log
 const cwd = process.cwd()
 
 module.exports = (async () => {
   const id = UUID.v4()
-  log('creating tmp')
 
   try {
-    await fs.mkdir(`${cwd}/tmp`)
+    await fs.mkdir(`${cwd}/bundle-cop`)
+    log('created bundle-cop directory')
   } catch (e) {
   }
 
-  log('running npm install')
   await exec(`npm install`)
-  log(`running build-analyze`)
+
   await exec(`npm run build-analyze`)
 
   const branchStats = await fs.readFile(`${cwd}/.next/stats.json`, 'utf8')
-  const branchStatsPath = path.resolve(cwd, 'tmp', 'branch-stats.json')
+  const branchStatsPath = path.resolve(cwd, 'bundle-cop', 'branch-stats.json')
 
   log(`writing branch-stats.json to ${branchStatsPath}`)
   await fs.writeFile(branchStatsPath, branchStats, 'utf8')
@@ -41,44 +38,29 @@ module.exports = (async () => {
   git.stash()
 
   git.checkout('next', async (err, data) => {
-    log(`rm -rf node_modules`)
-    await exec('rm -rf node_modules')
-
-    log(`npm install`)
     await exec('npm install')
 
-    log(`running build-analyze on master`)
     await exec(`npm run build-analyze`)
 
     const masterStats = await fs.readFile(`${cwd}/.next/stats.json`, 'utf8')
-    const masterStatsPath = path.resolve(cwd, 'tmp', 'master-stats.json')
+    const masterStatsPath = path.resolve(cwd, 'bundle-cop', 'master-stats.json')
 
     log(`writing master-stats.json to ${masterStatsPath}`)
     await fs.writeFile(masterStatsPath, masterStats, 'utf8')
 
-    log(`installing webpack-compare-pretty`)
     await exec('npm i webpack-compare-pretty -g')
 
-    log(`comparing`)
-    await exec(`webpack-compare ${branchStatsPath} ${masterStatsPath} -o ${cwd}/tmp`)
+    await exec(`webpack-compare ${branchStatsPath} ${masterStatsPath} -o ${cwd}/bundle-cop`)
 
-    // log(`openning headless chrome and taking screenshot`)
-    // await screenshot(`${cwd}/tmp/index.html`, `${cwd}/tmp/diff-${id}.png`)
-
-    log(`deleting ${branchStatsPath} ${masterStatsPath}`)
     await exec(`rm -rf ${branchStatsPath} ${masterStatsPath}`)
 
-    log(`deploying bundle..`)
-    const deploy = await exec(`now tmp`)
+    const deploy = await exec(`now bundle-cop`)
 
     log('Checking out previous branch')
 
     git.checkout(prevBranch, async (err, data) => {
-      log('Restoring git history')
-      git.stash('pop')
-
       log('removing temp files')
-      await exec(`rm -rf tmp`)
+      await exec(`rm -rf bundle-cop`)
 
       log(chalk.green(deploy.stdout))
     })
@@ -86,8 +68,10 @@ module.exports = (async () => {
 })()
 
 function exec (command, options = {}) {
+  log(command)
+
   return new Promise((done, failed) => {
-    cp.exec(command, { cwd, ...options}, (error, stdout, stderr) => {
+    cp.exec(command, { cwd, ...options }, (error, stdout, stderr) => {
       if (error) {
         error.stdout = stdout
         error.stderr = stderr
